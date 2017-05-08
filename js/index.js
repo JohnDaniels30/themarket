84933225b34c0d4c92a4a59badecac751dc8eaea
Eugene Burdeinyi <ojdgaf@gmail.com>
2017-05-08 16:17:49 +0300

    v1.0

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    /* Smooth Scrolling */
+
+    $('a[href*="#"]').click(function(event) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        if (target.length) {
+            event.preventDefault();
+
+            $('html, body').animate(
+              { scrollTop: target.offset().top - $('nav').outerHeight() }, 
+              1000, 
+              function() {
+                  var $target = $(target);
+                  $target.focus();
+                  if ($target.is(":focus")) {
+                    return false;
+                  } else {
+                    $target.attr('tabindex','-1');
+                    $target.focus();
+                  }
+            });
+        }
+    });
+    
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+<h1 align="center">themarket</h1>
+
+## About
+
+This course of laboratory works is intended to create a basic web store
+
+## Releases
+
+1.0 - HTML & CSS: creating home, authentication and registration pages
